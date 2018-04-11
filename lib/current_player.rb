0786604531d8420836a698e26dfaa375023a6be5
Halyette <haly.lesane@gmail.com>
2018-04-11 14:57:45 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
-board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
+turn_count = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
 counter = 0
-turn_count.each do |board|
+turn_count.each do |turn_count|
   puts "It's your turn #{counter}"
-  puts "O #{board}"
+  puts "O #{turn_count}"
   counter += 1 
 end  
